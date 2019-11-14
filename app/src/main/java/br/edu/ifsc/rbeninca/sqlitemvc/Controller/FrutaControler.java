2f57a2b875706bedbd2cd386341519a90726ac1f
rbeninca <rbeninca@gmail.com>
2019-11-14 00:40:59 -0300

    Implementação click com recycler view

--- app/src/main/java/br/edu/ifsc/rbeninca/sqlitemvc/Controller/FrutaControler.java
+++ app/src/main/java/br/edu/ifsc/rbeninca/sqlitemvc/Controller/FrutaControler.java
@@ -1,6 +1,7 @@
 package br.edu.ifsc.rbeninca.sqlitemvc.Controller;
 import android.content.Context;
 
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -12,19 +13,44 @@ public class FrutaControler {
         this.mContext = c;
         frutaDao = new FrutaDao(this.mContext);
     }
-    public  boolean cadastrar_fruta(Fruta f){
-        if (frutaDao.insere(f) >0)
-            return true;
-        else
-            return  false;
+    public  int excluir_fruta(int i ){
+        if (i>0) {
+            return frutaDao.delete_fruta(frutaDao.getFruta(i));
+        }
+        return -1;
     }
+    public  Fruta cadastrar_fruta(Fruta f){
+        if (f.getNome().length()>=3)
+            return frutaDao.insere(f);
+        return  null;
+
+    }
+
+    public int  atualizarFruta(Fruta f){
+        return     this.frutaDao.atualiza_fruta(f);
+    }
+
+    public Fruta getFruta(int id){
+        return frutaDao.getFruta(id);
+
+    }
+
+    public int excluir_fruta(Fruta f){
+        return   excluir_fruta(f);
+    };
+
 
     public ArrayList<Fruta> listaFrutas(){
         return  frutaDao.listaFrutas();
     };
 
+
     public ArrayList<String> listaNomesFrutas(){
         ArrayList<String> listaNomesFrutasResult= new ArrayList<String>();
+
+        for (Fruta fruta: this.listaFrutas() ){
+            listaNomesFrutasResult.add(fruta.getNome());
+        }
 
         Iterator<Fruta> it = this.listaFrutas().iterator();
         while (it.hasNext()){
@@ -33,5 +59,7 @@ public class FrutaControler {
         }
         return  listaNomesFrutasResult;
     }
+
+
 
 }
--- app/src/main/java/br/edu/ifsc/rbeninca/sqlitemvc/View/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/rbeninca/sqlitemvc/View/MainActivity.java
@@ -4,56 +4,109 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 import br.edu.ifsc.rbeninca.sqlitemvc.Controller.FrutaControler;
 import br.edu.ifsc.rbeninca.sqlitemvc.Model.Fruta;
 import br.edu.ifsc.rbeninca.sqlitemvc.R;
+import br.edu.ifsc.rbeninca.sqlitemvc.Util.AdapterFrutaListagem;
+import br.edu.ifsc.rbeninca.sqlitemvc.Util.RecyclerItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
     EditText id, nome , tipo;
-    ListView listView ;
+    RecyclerView  recyclerView ;
     FrutaControler frutaControler;
+    Button buttonSalvar;
+    ArrayList<Fruta> frutaArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.frutaControler = new FrutaControler( getApplicationContext());
+
         this.id=findViewById(R.id.editText_id);
         this.nome=findViewById(R.id.editText_nome);
         this.tipo=findViewById(R.id.editText_tipo);
-        this.listView=findViewById(R.id.list_view);
+        this.recyclerView=findViewById(R.id.recyclerView);
+        this.buttonSalvar=findViewById(R.id.buttonSalvar);
 
-        this.frutaControler = new FrutaControler( getApplicationContext());
-        loadListagem();
-
+        this.recyclerView.addOnItemTouchListener(recyclerItemClickListener);
     }
 
-    public  void salvar(View v){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadListagem();
+    }
 
-        if (frutaControler.cadastrar_fruta( new Fruta(null,nome.getText().toString(), tipo.getText().toString()) ))
-            Toast.makeText(this,"Fruta Salva",Toast.LENGTH_LONG).show();
-        else
-            Toast.makeText(this,"Falha ao Salvar",Toast.LENGTH_LONG).show();
+    public RecyclerItemClickListener recyclerItemClickListener= new RecyclerItemClickListener(
+                    getBaseContext(),
+                    recyclerView,
+                    new RecyclerItemClickListener.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(View view, int position) {
+                            Fruta f=frutaControler.getFruta(frutaArrayList.get(position).getId());
+                            id.setText(f.getId().toString());
+                            nome.setText(f.getNome());
+                            tipo.setText(f.getTipo());
+                            Toast.makeText(getApplicationContext(),Integer.toString(position),Toast.LENGTH_LONG).show();
+                        }
+                        @Override
+                        public void onLongItemClick(View view, int position) { }
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) { }
+                    });
+
+
+    public ListView.OnItemClickListener clickItemListView = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long ide) {
+            Fruta f=frutaControler.getFruta(position);
+            id.setText(f.getId().toString());
+            nome.setText(f.getNome());
+            tipo.setText(f.getTipo());
+            Toast.makeText(getApplicationContext(),Integer.toString(position),Toast.LENGTH_LONG).show();
+        }
+    };
+
+    public  void salvar(View v){
+        String resultado;
+        if (!id.getText().toString().trim().isEmpty()){
+            frutaControler.atualizarFruta(new Fruta(Integer.parseInt(id.getText().toString()),nome.getText().toString(), tipo.getText().toString()) );
+        }else{
+            frutaControler.cadastrar_fruta(new Fruta(nome.getText().toString(), tipo.getText().toString())  );
+        }
         loadListagem();
     }
 
     public  void loadListagem(){
-         /*
-            Configurando o listView
-         */
-        ArrayAdapter<String> adapter =  new ArrayAdapter(
+         /*  Configurando o listView  */
+
+        frutaArrayList =this.frutaControler.listaFrutas();
+        AdapterFrutaListagem adapterFrutaListagem = new AdapterFrutaListagem(
                 getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                frutaControler.listaNomesFrutas()
+                R.layout.layout_item_lista,
+                frutaArrayList
         );
-
-        listView.setAdapter(adapter);
-
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this); //new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(
+                new DividerItemDecoration(
+                        recyclerView.getContext(),
+                        DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(adapterFrutaListagem);
     }
 }
